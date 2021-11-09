b669af3b51f95392a3d64adae9dca0bd051e130d
gabriel barbosa <gbjerl03@gmail.com>
2021-11-09 14:47:31 -0300

    Client init

---
+++ client/src/pages/Favorits.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+
+export default function Favorits() {
+
+  return (
+    <>
+      <View>
+        <Text>Olá</Text>
+      </View>
+    </>
+  )
+
+}
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+
+export default function Home() {
+
+  return (
+    <>
+      <View>
+        <Text>Olá</Text>
+      </View>
+    </>
+  )
+
+}
